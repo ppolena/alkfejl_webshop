56c31c444383bccfc10aa9440087c536b750953c
ppolena <PPolena92@gmail.com>
2018-12-08 17:17:24 +0100

    Added endpoint for getting logged in user

--- src/main/java/alkfejl_webshop/controller/UserController.java
+++ src/main/java/alkfejl_webshop/controller/UserController.java
@@ -8,9 +8,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.security.Principal;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -27,6 +36,16 @@ public class UserController{
     @GetMapping
     public ResponseEntity<List<User>> getAllUsers(){
         return ResponseEntity.status(HttpStatus.OK).body(userRepository.findAll());
+    }
+
+    @Secured({"ROLE_CUSTOMER"})
+    @GetMapping("/user")
+    public ResponseEntity<User> getLoggedInUser(Principal principal) {
+        Optional<User> user = userRepository.findByEmail(principal.getName());
+        if (user.isPresent()) {
+            return ResponseEntity.status(HttpStatus.OK).body(user.get());
+        }
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
     @Secured({"ROLE_ADMIN"})
